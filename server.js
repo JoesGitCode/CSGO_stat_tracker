814e484d7a98a386c0243eb3cc9002c02392eb4a
JoesGitCode <jsyoung0891@gmail.com>
2019-09-23 17:41:31 +0100

    server returns a response

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 
 app.get("/api/v2/profile/steam/:steamid", async (req, res) => {
-  const headers = { "TRN-Api-Key": process.env.API_KEY };
-  const apiUrl = process.env.API_URL;
-  const { steamid } = req.params;
+
+  res.send('heloo')
+
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
