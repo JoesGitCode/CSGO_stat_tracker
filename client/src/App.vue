28358ddc0d9d1b6ac5e9b6a7eb164bbff06097e9
JoesGitCode <jsyoung0891@gmail.com>
2019-09-24 16:15:27 +0100

    added vue and font familty

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">text</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "csgo_font";
+  src: URL("../public/cs_regular.ttf") format("truetype");
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "csgo_font";
+}
+</style>
